bda584953098f871b3c8ab7566cebe3291adbef7
Victoria Nacul <victorianacul15@gmail.com>
2023-02-25 19:43:02 -0300

    Primer escritura en el repositorio

---
+++ js/funcion.js
@@ -0,0 +1,13 @@
+
+document.getElementById('boton').onclick = function () {
+console.log("Gracias Profesor Daniel Rinaldi");
+document.getElementById("demo").innerHTML = "Muchas Gracias, por visitar Curriculum Vitae_Proyecto Ticmas";
+}
+
+document.getElementById('boton_color').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'cadetblue';
+});
+
+document.getElementById('boton_default').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'white';
+});
