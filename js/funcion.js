7428db9d2e93e4d2b608492b14bfb90398755c14
Victoria Nacul <victorianacul15@gmail.com>
2023-03-25 00:46:05 -0300

    pruebas y errores

--- js/funcion.js
+++ js/funcion.js
@@ -9,5 +9,5 @@ document.getElementById('boton_color').addEventListener('click', function () {
 });
 
 document.getElementById('boton_default').addEventListener('click', function () {
-    document.body.style.backgroundColor = 'white';
+    document.body.style.backgroundColor = 'blanchedalmond';
 });
